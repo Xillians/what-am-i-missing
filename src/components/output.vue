f69d0866c2452e15f0b891ea1dcae707cde31f17
Xillians <chkxillians@gmail.com>
2021-09-30 18:04:57 +0200

    beautify output

--- src/components/output.vue
+++ src/components/output.vue
@@ -1,31 +1,71 @@
 <template>
-  <div id="output" class="output">
-    <!-- decklist: {{decklist}} <br>
-      collection: {{collection}} <br> -->
-    <li 
-        class="outputList"
-        v-for="card in outputText" 
-        :key="card"
-    >{{card}}</li>
+  <div class="output" v-if="showOutput">
+    <textarea
+      id="output"
+      class="textArea"
+      :value="formattedText"
+      readonly
+    />
+    <base-button id="`copy`" text="clipboard" @click="copyToClipboard"/>
   </div>
 </template>
 
 <script>
+import baseButton from "./basic components/btn.vue";
+
 export default {
   name: "outputData",
   props: ["decklist", "collection", "outputText"],
+  components: { baseButton },
+  watch: {
+    outputText: function () {
+      if (!this.showOutput) this.showOutput = true;
+      this.formattedText = this.outputText.join("\n");
+    },
+  },
+  data() {
+    return {
+      showOutput: false,
+      formattedText: "output",
+    };
+  },
+  methods: {
+    copyToClipboard() {
+      var output = document.getElementById("output");
+
+      output.select();
+      output.setSelectionRange(0, 999999);
+      navigator.clipboard.writeText(output.value);
+
+      alert("copied to clipboard!");
+    },
+  },
 };
 </script>
 
 <style scoped>
 .output {
   position: relative;
-  width: 90%;
   height: 40%;
-  margin-top: 30px;
+  margin-top: 20px;
 }
-.outputList {
-    position: relative;
-    display: block;
+.textArea {
+  width: 40%;
+  height: 75px;
+  position: relative;
+  margin-bottom: 10px;
+  margin-top: 5px;
+  resize: none;
+  animation: 0.5s fadeIn;
+  animation-fill-mode: forwards;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    visibility: visible;
+    opactiy: 1;
+  }
 }
 </style>
