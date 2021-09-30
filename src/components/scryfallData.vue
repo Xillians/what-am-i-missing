f08c27fbcc98e175cd8cfa94ac866cdedac94805
Xillians <chkxillians@gmail.com>
2021-09-30 22:16:52 +0200

    debug values

--- src/components/scryfallData.vue
+++ src/components/scryfallData.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div>{{cardData}}</div>
 </template>
 
 <script>
@@ -67,7 +67,7 @@ export default {
   watch: {
     outputText: async function () {
       console.log("woop");
-      await this.getMultipleCards();
+      // await this.getMultipleCards();
       this.$emit("onCardInfoUpdated", this.cardData);
     },
   },
