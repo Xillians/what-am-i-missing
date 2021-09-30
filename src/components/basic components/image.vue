b6449a3aa2e5576aed215002c61e7aa7688d7685
Xillians <chkxillians@gmail.com>
2021-09-30 19:21:12 +0200

    create base image class

---
+++ src/components/basic components/image.vue
@@ -0,0 +1,29 @@
+<template>    
+    <img :src="source"/>
+</template>
+
+<script>
+export default {
+    name: "baseImage",
+    props: ["source"]
+}
+</script>
+
+
+<style scoped>
+.input {
+  position: relative;
+  resize: none;
+  animation: .5s fadeIn;
+  animation-fill-mode: forwards;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    visibility: visible;
+    opactiy: 1;
+  }
+}
+</style>
