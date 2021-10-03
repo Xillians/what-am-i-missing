8f713929b94939f81afb10e0f44b912a2d991ef5
Xillians <chkxillians@gmail.com>
2021-10-04 01:53:07 +0200

    re-do for output

--- src/components/scryfallData.vue
+++ src/components/scryfallData.vue
@@ -16,8 +16,8 @@ export default {
   },
   methods: {
     async fetchScryfallData() {
-      let cards = this.removeAmountFromList(this.outputText);
-      let formattedList  = [];
+      let cards = this.splitAmountAndNameList(this.outputText);
+      let formattedList = [];
 
       while (cards.length > 75) {
         let block = cards.splice(0, 75);
@@ -42,30 +42,47 @@ export default {
         image: jsonData.image_uris.small,
         rarity: jsonData.rarity,
         prices: jsonData.prices,
+        qty: 0,
       };
     },
     async getFullCardListInfo(block, cardList) {
-      let response = await this.scryfall.POST_Collection(block);
+      let names = [];
+      block.forEach((object) => {
+        const requestObject = {
+          name: object.card.name,
+        };
+        names.push(requestObject);
+      });
+      let response = await this.scryfall.POST_Collection(names);
       let blockCardList = response.data.data;
 
       for (const card of blockCardList) {
-        const cardInfo = await this.convertDataToInfo(card);
+        let cardInfo = await this.convertDataToInfo(card);
+        cardInfo = this.setQuantity(cardInfo, block);
         cardList.push(cardInfo);
       }
       return cardList;
     },
-    removeAmountFromList(cardList = []) {
+    setQuantity(formattedCardInfo, block) {
+      const search = (element) => element.card.name.toLowerCase() === formattedCardInfo.name.toLowerCase();
+      const cardInBlock = block.find(search);
+      formattedCardInfo.qty = cardInBlock.card.quantity;
+      return formattedCardInfo;
+    },
+    splitAmountAndNameList(cardList = []) {
       const formattedList = [];
       cardList.forEach((card) => {
-        const cardName = this.removeAmount(card);
-        formattedList.push({ name: cardName });
+        const formattedData = this.splitAmountAndName(card);
+        formattedList.push({ card: formattedData });
       });
       return formattedList;
     },
-    removeAmount(text) {
+    splitAmountAndName(text) {
       const split = text.split(" ");
+      const quantity = Number(split[0]);
       split.shift();
-      return split.join(" ");
+      const name = split.join(" ");
+      return { name, quantity };
     },
   },
   watch: {
