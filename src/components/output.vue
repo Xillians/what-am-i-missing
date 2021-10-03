86a50f2bce8a7df16bf6b70ccc0d337498644b42
Xillians <chkxillians@gmail.com>
2021-10-04 01:53:20 +0200

    output now shows scryfall data!

--- src/components/output.vue
+++ src/components/output.vue
@@ -1,33 +1,73 @@
 <template>
   <div class="output" v-if="showOutput">
-    <textarea
-      id="output"
-      class="textArea"
-      :value="formattedText"
-      readonly
-    />
-    <base-button id="`copy`" text="clipboard" @click="copyToClipboard"/>
-    {{scryfallInfo}}
+    <textarea id="output" class="textArea" :value="formattedText" readonly />
+    <base-button id="`copy`" text="clipboard" @click="copyToClipboard" />
+    <div id="rarities">
+      commons: {{ rarities.common }} <br />
+      uncommons: {{ rarities.uncommon }} <br />
+      rares: {{ rarities.rare }} <br />
+      mythic rares: {{ rarities.mythic }} <br />
+      Estimated price: <br>
+      ${{prices.usd}} <br>
+      €{{prices.eur}}
+    </div>
+    <div class="imageBox">
+      <div v-for="object in scryfallInfo" v-bind:key="object.image">
+        <baseImage :source="object.image" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import baseButton from "./basic components/btn.vue";
+import baseImage from "./basic components/image.vue";
 
 export default {
   name: "outputData",
   props: ["outputText", "scryfallInfo"],
-  components: { baseButton },
+  components: { baseButton, baseImage },
   watch: {
     outputText: function () {
       if (!this.showOutput) this.showOutput = true;
       this.formattedText = this.outputText.join("\n");
+      this.resetData();
+    },
+    scryfallInfo: function () {
+      let outputWithSets = [];
+      this.scryfallInfo.forEach((card) => {
+        this.rarities[card.rarity] += card.qty;
+        let prices = this.prices;
+        prices.usd = this.updatePrice(prices.usd, card.prices.usd);
+        prices.usd_foil = this.updatePrice(prices.usd_foil, card.prices.usd_foil);
+        prices.usd_etched = this.updatePrice(prices.usd_etched, card.prices.usd_etched);
+        prices.eur = this.updatePrice(prices.eur, card.prices.eur);
+        prices.eur_foil = this.updatePrice(prices.eur_foil, card.prices.eur_foil);
+        prices.tix = this.updatePrice(prices.tix, card.prices.tix);
+        const formattedTextInfo = `${card.qty} ${card.name} [${card.sets[0].name}]`;
+        outputWithSets.push(formattedTextInfo);
+      });
+      this.formattedText = outputWithSets.join('\n');
     },
   },
   data() {
     return {
       showOutput: false,
       formattedText: "output",
+      rarities: {
+        common: 0,
+        uncommon: 0,
+        rare: 0,
+        mythic: 0,
+      },
+      prices: {
+        usd: 0,
+        usd_foil: 0,
+        usd_etched: 0,
+        eur: 0,
+        eur_foil: 0,
+        tix: 0
+      }
     };
   },
   methods: {
@@ -40,11 +80,37 @@ export default {
 
       alert("copied to clipboard!");
     },
+    updatePrice(originalValue, newValue) {
+      if(newValue != null) {
+        const number = Number(newValue);
+        originalValue += number;
+      }
+      return Math.round(originalValue * 100) / 100;
+    },
+    resetData() {
+      this.rarities.common = 0;
+      this.rarities.uncommon = 0;
+      this.rarities.rare = 0;
+      this.rarities.mythic = 0;
+      this.prices.usd = 0;
+      this.prices.usd_foil = 0;
+      this.prices.usd_etched = 0;
+      this.prices.eur = 0;
+      this.prices.eur_foi = 0;
+      this.prices.usd = 0;
+    }
   },
 };
 </script>
 
 <style scoped>
+.imageBox {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  width: auto;
+}
 .output {
   position: relative;
   height: 40%;
@@ -52,7 +118,7 @@ export default {
 }
 .textArea {
   width: 40%;
-  height: 75px;
+  height: 120px;
   position: relative;
   margin-bottom: 10px;
   margin-top: 5px;
