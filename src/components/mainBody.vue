520c8c36b86bb3924026472209b8f26b031a0176
Xillians <chkxillians@gmail.com>
2021-10-07 21:26:48 +0200

    set inputs to be flex

--- src/components/mainBody.vue
+++ src/components/mainBody.vue
@@ -1,49 +1,47 @@
 <template>
   <div>
-    <inputs :name="`decklist`" @ondecklistUpdate="decklist = $event" />
-    <inputs :name="`collection`" @oncollectionUpdate="collection = $event" />
+    <div id="inputs" class="flexbos">
+      <inputs :name="`decklist`" @ondecklistUpdate="decklist = $event" />
+      <inputs :name="`collection`" @oncollectionUpdate="collection = $event" />
+    </div>
     <compareData
       :decklist="decklist"
       :collection="collection"
       @onDataCompared="outputResult = $event"
     />
-    <outputData 
-      :outputText="outputResult"
-      :scryfallInfo="scryfallInfo"
-    />
-    <scryfall-data :outputText="outputResult" @onCardInfoUpdated="something"/>
+    <outputData :outputText="outputResult" :scryfallInfo="scryfallInfo" />
+    <scryfall-data :outputText="outputResult" @onCardInfoUpdated="something" />
   </div>
 </template>
 
 <script>
-import inputs from './basic components/inputs.vue';
-import compareData from './compareData.vue';
-import outputData from './output.vue';
-import scryfallData from './scryfallData.vue'
+import inputs from "./basic components/inputs.vue";
+import compareData from "./compareData.vue";
+import outputData from "./output.vue";
+import scryfallData from "./scryfallData.vue";
 
 export default {
   name: "mainBody",
   data() {
     return {
-      decklist:"",
-      collection:"",
-      outputResult:"",
-      scryfallInfo:[]
-    }
+      decklist: "",
+      collection: "",
+      outputResult: "",
+      scryfallInfo: [],
+    };
   },
   components: {
-      inputs,
-      compareData,
-      outputData,
-      scryfallData
+    inputs,
+    compareData,
+    outputData,
+    scryfallData,
   },
   methods: {
     something(eventData) {
       this.scryfallInfo = eventData;
-    }
+    },
   },
-  props: [
-  ]
+  props: [],
 };
 </script>
 
