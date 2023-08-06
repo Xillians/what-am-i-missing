0fdc3acbed73c0e9f9dbf5ff430b879f8bcb2979
xillians <sandra.toftemo@gmail.com>
2023-08-06 23:55:10 +0200

    try hard coding public path?

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.PUBLICPATH ??  "/",
+  publicPath: "/what-am-i-missing/",
 });
