f85ca5d4166cc065dd52aab4c4764e2fa4940f6a
xillians <sandra.toftemo@gmail.com>
2023-08-07 22:00:35 +0200

    implement base view of card data

---
+++ src/components/card-data.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card-data">
+      <b>{{ card.name }}</b>
+      <img :src="card.image_uris.normal" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { CardData } from "@/models/card-data";
+
+@Options({
+  name: "cardData",
+  props: {
+    card: {
+      type: Object as () => CardData,
+      required: true,
+    },
+  },
+})
+export default class cardData extends Vue {}
+</script>
+
+<style scoped>
+.card-data {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+img {
+  width: 223px;
+  height: 310px;
+}
+</style>
