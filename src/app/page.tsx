f5ff3c273e133a9ce2e46813646b6006322739bd
xillians <sandra.toftemo@gmail.com>
2023-11-10 23:24:53 +0100

    finalize step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
 import React from "react";
+import CardInputs from "./_components/input-boxes/card-inputs";
+import OutputBox from "./_components/output-data/output-box";
+import { Flex } from "@chakra-ui/react";
+import Output from "./_components/output-data/output";
 
 function Page() {
+  const [cards, setMissingCards] = React.useState<Map<string, number>>(new Map<string, number>());
+  
   return (
-    <div>
-      <h1>Page</h1>
-    </div>
+    <Flex 
+      flexDir={"column"}
+      width={{ base: "100%", md: "50%" }}
+      >
+      <CardInputs setMissingCards={setMissingCards} />
+      {
+        cards.size > 0 &&
+        <Output cards={cards} />
+      }
+    </Flex>
   );
 }
 
