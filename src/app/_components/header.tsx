15983383e5fcfdfc1d703b0efb04b457f6bbde2e
xillians <sandra.toftemo@gmail.com>
2023-11-10 20:19:34 +0100

    use next.js

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@chakra-ui/react';
+import './header.scss';
+
+export default function Header() {
+  return (
+    <Heading as="h1" size="xl" className="header">
+        Magic  gap analyzer
+    </Heading>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+// create root page
+
+import React from "react";
+import { metadata } from "./layout";
+
+export default function RootPage({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
