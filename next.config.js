8efd53c11ae9caa65b096d181a1b9834c23af399
xillians <sandra.toftemo@gmail.com>
2023-11-11 04:01:14 +0100

    modern is truu

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  target: 'experimental-serverless-trace',
+  // other configurations...
+
+  experimental: {
+    // enable modern mode for smaller, faster, and more efficient production builds
+    modern: true,
+    // enable server components (experimental feature)
+    serverComponents: true,
+  },
+
+  // export the site as static HTML
   output: 'export',
 };
 
